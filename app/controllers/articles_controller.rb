ff5a2c39092f7569be6c97129561503b4f2021f3
Joao Rodrigues <JoaoRodrigues@Joaos-Air-2.lan>
2016-01-25 00:24:55 +0000

    Add edit, update, index and show controllers, views and db functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,17 @@
 require 'pp'
 class ArticlesController < ApplicationController
+  
+  #PAGE LOAD
   def new
     @article = Article.new
   end
 
+  #INDEX
+  def index
+    @articles = Article.all
+  end
+
+  #NEW ARTICLE
   def create
     @article = Article.new(article_params)
     if @article.save then
@@ -17,8 +25,25 @@ class ArticlesController < ApplicationController
     end
   end
 
+  #SHOW ARTICLE
   def show
     @article = Article.find(params[:id])
+  end
+
+  #EDIT
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  #UPDATE
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params) then
+      flash[:notice] = 'Successfully updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
 
